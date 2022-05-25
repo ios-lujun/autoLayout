d0ea2bd69ba34f40f48445653eeacaff654b6da0
johnson <417537612@qq.com>
2022-05-25 17:26:03 +0800

    ep6

--- autolayout/SwipingController.swift
+++ autolayout/SwipingController.swift
@@ -17,7 +17,57 @@ class SwipingController: UICollectionViewController,UICollectionViewDelegateFlow
         .init(imageName: "bear_first", headerText: "加入我们，游戏和乐趣", bodyText: "你准备好迎接大量的乐趣了吗？不要再等了！我们希望很快能在我们的商店见到您."),
         .init(imageName: "heart_second", headerText: "订阅并获得我们日常活动的优惠券", bodyText: "当我们在我们的网站上宣布节省时，立即得到通知。确保也向我们提供您的任何反馈."),
         .init(imageName: "leaf_third", headerText: "VIP会员专属服务.", bodyText: ""),
+        .init(imageName: "bear_first", headerText: "加入我们，游戏和乐趣", bodyText: "你准备好迎接大量的乐趣了吗？不要再等了！我们希望很快能在我们的商店见到您."),
+        .init(imageName: "heart_second", headerText: "订阅并获得我们日常活动的优惠券", bodyText: "当我们在我们的网站上宣布节省时，立即得到通知。确保也向我们提供您的任何反馈."),
+        .init(imageName: "leaf_third", headerText: "VIP会员专属服务.", bodyText: ""),
     ]
+    
+    private lazy var  previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("上一页", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handlerPrevButton), for: .touchUpInside)
+        return button
+    }()
+    
+       private lazy var  nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("下一页", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainPink, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handlerNextButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func handlerPrevButton(){
+        let index = max(self.page.currentPage - 1,0)
+        let indexPath = IndexPath(item: index, section: 0)
+        self.page.currentPage = index
+        collectionView.scrollToItem(at: indexPath, at: .right, animated: true)
+    }
+    
+    @objc private func handlerNextButton(){
+        let index = min(self.page.currentPage + 1, pages.count - 1)
+        let indexPath = IndexPath(item: index, section: 0)
+        self.page.currentPage = index
+        collectionView.scrollToItem(at: indexPath, at: .right, animated: true)
+    }
+    
+   private lazy var page: UIPageControl = {
+        let page = UIPageControl()
+        page.currentPage = 0
+        page.numberOfPages = pages.count
+        page.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        page.currentPageIndicatorTintColor = .mainPink
+        page.allowsContinuousInteraction = false
+        page.translatesAutoresizingMaskIntoConstraints = false
+        return page
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +76,7 @@ class SwipingController: UICollectionViewController,UICollectionViewDelegateFlow
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: PageCell.cellId)
         collectionView.isPagingEnabled = true
 //        collectionView.showsHorizontalScrollIndicator = false
+        addComponseButtons()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat { 0 }
@@ -42,7 +93,34 @@ class SwipingController: UICollectionViewController,UICollectionViewDelegateFlow
         CGSize(width: view.frame.width, height: view.frame.height)
     }
     
-
+    //MARK: - 代理
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let x = targetContentOffset.pointee.x
+        self.page.currentPage = Int(x / view.frame.width)
+    }
+    
+//    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        let x = scrollView.contentOffset.x
+//        self.page.currentPage = Int(x / view.frame.width)
+//    }
+    
 }
 
+
+
+extension SwipingController{
+    private func addComponseButtons(){
+        let buttonControlsStackView = UIStackView(arrangedSubviews: [previousButton,page,nextButton])
+        buttonControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonControlsStackView.distribution = .fillEqually
+        view.addSubview(buttonControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            buttonControlsStackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            buttonControlsStackView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            buttonControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
 
