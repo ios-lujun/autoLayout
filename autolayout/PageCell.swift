181e743b8e814806e407135e373260be23923df0
johnson <417537612@qq.com>
2022-05-25 16:35:13 +0800

    luadasd

--- autolayout/PageCell.swift
+++ autolayout/PageCell.swift
@@ -11,6 +11,12 @@ class PageCell: UICollectionViewCell {
     
     static let cellId = "SwipingController"
     
+//    var page:: Page? {
+//        didSet{
+//
+//        }
+//    }
+    
     let imageView: UIImageView = {
         let iv = UIImageView(image: UIImage(named: "bear_first"))
         iv.translatesAutoresizingMaskIntoConstraints = false
@@ -21,9 +27,9 @@ class PageCell: UICollectionViewCell {
     let textView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
-        let mutableAttributedString = NSMutableAttributedString(string: "加入我们，游戏和乐趣", attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 18)])
+       /* let mutableAttributedString = NSMutableAttributedString(string: "加入我们，游戏和乐趣", attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 18)])
         mutableAttributedString.append(.init(string: "\n\n\n你准备好迎接大量的乐趣了吗？不要再等了！我们希望很快能在我们的商店见到您.", attributes: [NSAttributedString.Key.foregroundColor:UIColor.gray,NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 12)]))
-        textView.attributedText = mutableAttributedString
+        textView.attributedText = mutableAttributedString*/
         textView.isEditable = false
         textView.isScrollEnabled = false
         textView.textAlignment = .center
@@ -49,6 +55,18 @@ class PageCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    public func configuare(with pageModel: Page?){
+        guard let pageModel = pageModel else {
+            return
+        }
+        self.imageView.image = UIImage(named: pageModel.imageName)
+        
+        let mutableAttributedString = NSMutableAttributedString(string:pageModel.headerText, attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 18)])
+        mutableAttributedString.append(.init(string: "\n\n\n\(pageModel.bodyText)", attributes: [NSAttributedString.Key.foregroundColor:UIColor.gray,NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 13)]))
+        
+        self.textView.attributedText = mutableAttributedString
+    }
+    
 }
 
 
@@ -60,9 +78,9 @@ extension PageCell{
         contentView.addSubview(topImageViewContainer)
         NSLayoutConstraint.activate([
             topImageViewContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
-            topImageViewContainer.leftAnchor.constraint(equalTo: contentView.leftAnchor),
-            topImageViewContainer.rightAnchor.constraint(equalTo: contentView.rightAnchor),
-            topImageViewContainer.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.5)
+            topImageViewContainer.leftAnchor.constraint(equalTo: leftAnchor),
+            topImageViewContainer.rightAnchor.constraint(equalTo: rightAnchor),
+            topImageViewContainer.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5)
             
         ])
         
@@ -80,5 +98,4 @@ extension PageCell{
             textView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
         ])
     }
-
 }
--- autolayout/SwipingController.swift
+++ autolayout/SwipingController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+struct Page{
+    var imageName,headerText,bodyText: String
+}
+
 class SwipingController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
    
-    
+    var pages: [Page] = [
+        .init(imageName: "bear_first", headerText: "加入我们，游戏和乐趣", bodyText: "你准备好迎接大量的乐趣了吗？不要再等了！我们希望很快能在我们的商店见到您."),
+        .init(imageName: "heart_second", headerText: "订阅并获得我们日常活动的优惠券", bodyText: "当我们在我们的网站上宣布节省时，立即得到通知。确保也向我们提供您的任何反馈."),
+        .init(imageName: "leaf_third", headerText: "VIP会员专属服务.", bodyText: ""),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,16 +25,16 @@ class SwipingController: UICollectionViewController,UICollectionViewDelegateFlow
         
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: PageCell.cellId)
         collectionView.isPagingEnabled = true
-        collectionView.showsHorizontalScrollIndicator = false
+//        collectionView.showsHorizontalScrollIndicator = false
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat { 0 }
     
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { 4 }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { pages.count }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PageCell.cellId, for: indexPath) as! PageCell
-//        cell.backgroundColor = indexPath.row % 2 == 0 ? .red : .blue
+        cell.configuare(with: pages[indexPath.item])
         return cell
     }
     
